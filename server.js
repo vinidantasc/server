9ac29705b5ab91e87d2c277e8cadce7044137c23
vinicius <vinicius.carvalho@ufrn.br>
2024-11-02 00:35:34 -0300

    inserido também captura do ipv4

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const port = 3000; // Você pode mudar a porta se quiser
 
 // Middleware para capturar o IP
 app.use((req, res, next) => {
+    // Captura do IP
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log(`IP Capturado: ${ip}`);
+
+    // Para IPv6, pode ser necessário remover o prefixo "::ffff:" 
+    const ipv4 = ip.includes('::ffff:') ? ip.replace('::ffff:', '') : ip;
+
+    console.log(`IP Capturado: ${ipv4}`);
     
     // Salvar IP em um arquivo
-    fs.appendFile('ips.txt', `${ip}\n`, (err) => {
+    fs.appendFile('ips.txt', `${ipv4}\n`, (err) => {
         if (err) {
             console.error('Erro ao salvar o IP:', err);
         }
